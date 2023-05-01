bbc1b72db495133cfe5012f1fbd27466535348f4
Bobur96 <boburusmonxojayev@gmail.com>
2023-05-01 10:15:24 +0500

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
-import Login from "./components/Login"
+import Doctor from './components/Doctor';
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />}/>
           <Route path='/register' element={<Register />}/>
+          <Route path='/doctor' element={<Doctor />}/>
         </Routes>
       </div>
     </BrowserRouter>
