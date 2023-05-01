fb7c9c202207e8d9c7b7bbd6c02ffc39569e12a2
Bobur96 <boburusmonxojayev@gmail.com>
2023-05-01 08:19:07 +0500

    last commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -18,14 +18,14 @@ const inpt2 = [48, 47, 46, 45, 44, 43, 42, 41, 31, 32, 33, 34, 35, 36, 37, 38];
 
 const Register = () => {
 
+  const queryAll = document.querySelectorAll.bind(document);
+  const query = document.querySelector.bind(document);
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
 
   const closeRef = useRef();
   const closeRef2 = useRef();
   const [doc, setDoc] = useState('');
-  const [doc2, setDoc2] = useState('');
-  const [patient2, setPatient2] = useState('')
 
   const [patient, setPatient] = useState([])
   const [doctors, setDoctors] = useState([])
@@ -43,12 +43,52 @@ const Register = () => {
     response()
   }, [])
 
-  const handleSubmit = () => {
-    document.querySelectorAll('input').forEach(el => {
-      el.checked = false;
+  const handleSubmit = async () => {
+    let patientAnswer = '';
+    let tablee = '';
+    const toothAnswer = [];
+    const deteilAnswer = [];
+    queryAll('.patient input').forEach(el => {
+      if(el.checked) patientAnswer = el.value;
     })
-    setDoc('')
-    toast.success('Successfully added!')
+    queryAll('.tooth input').forEach(el => {
+      if(el.checked) toothAnswer.push(el.value)
+    })
+    queryAll('.details_inp').forEach(el => {
+      if(el.checked) deteilAnswer.push(el.value)
+    })
+    
+    if(doc !== '' && patientAnswer !== '') {
+      console.log('aa')
+      await fetch(`${process.env.REACT_APP_API_URL}/register/create_treatmentteeth?patient_id=${patientAnswer}&attached_id=${doc}`,{
+        method: "POST",
+        headers: { token: sessionStorage.getItem('token') }
+      }).then(res => res.json())
+      .then(res => tablee = res )
+      .catch( err => toast.error('Ann error occured!'))
+
+      await fetch(`${process.env.REACT_APP_API_URL}/register/create_treatmentteeth?patient_id=${patientAnswer}&attached_id=${doc}`,{
+        method: "POST",
+        headers: { token: sessionStorage.getItem('token') },
+        body: JSON.stringify([{
+          treatmentteeth: tablee,
+          tooth_id: toothAnswer,
+          complaint_id: deteilAnswer,
+        }])
+      }).then(res => res.json())
+      .then(res => tablee = res )
+      .catch( err => toast.error('Ann error occured!'))
+        closeRef2.current.click();
+        queryAll('input').forEach(el => {
+          el.checked = false;
+        }); setDoc('')
+    }
+    else {
+      toast.error('All field required!')
+    }
+
+
+
   }
 
   const getPatiens = () => {
@@ -73,25 +113,19 @@ const Register = () => {
   }
 
   const handleSave = () => {
-    closeRef.current.click();
-    document.querySelectorAll("input").forEach((el) => (el.checked = false));
-  };
+    fetch(`${process.env.REACT_APP_API_URL}/register/create_patient`,{
+      method: "POST",
+      headers: { token: sessionStorage.getItem('token') },
+      body: JSON.stringify({
+        first_name: query('#first_name').value,
+        last_name: query('#last_name').value,
+        address: query('#address').value,
+        phone_number: query('#phone').value,
+      })
+    }).then(res => res.json())
+    .then(res => closeRef.current.click())
+    .catch( err => toast.error('Ann error occured!'))
 
-  const handleSaveCart = () => {
-    if(doc2 !== '' && patient2 !== '') {
-        fetch(`${process.env.REACT_APP_API_URL}/register/create_treatmentteeth?patient_id=${patient2}&attached_id=${doc2}`,{
-          method: "POST",
-          headers: { token: sessionStorage.getItem('token') }
-        }).then(res => {
-          toast.success('Succesfull added!'); 
-          closeRef2.current.click();
-          setPatient2(''); setDoc2('')
-        })
-        .catch( err => toast.error('Ann error occured!'))
-    }
-    else {
-      toast.error('All field required!')
-    }
   };
 
   const handleClick = (event) => {
@@ -112,8 +146,8 @@ const Register = () => {
             <>
               {
                 patient.map((item, i) => (
-                  <span className="list__item" key={i}>
-                    <input type="checkbox" id={`${item.id}`} value={item.id} />
+                  <span className="list__item patient" key={i}>
+                    <input type="radio" name="patient" id={`${item.id}`} value={item.id} />
                     <label htmlFor={`${item.id}`}>{item.first_name+'  '+item.last_name}</label>
                     <div className="menu__btn">
                       <IconButton aria-haspopup="true" onClick={handleClick}>
@@ -136,14 +170,7 @@ const Register = () => {
           
         </div>
         <div className="col-9 row reg_content">
-          <div className="col-8 reg_center" style={{ background: "#8BB6B3" }}>
-            <button className="btn btn-primary position-absolute"
-              style={{ top: "20px", left: "25px" }}
-              data-bs-target="#exampleModal2" 
-              data-bs-toggle="modal"
-            >
-              Create Cart
-            </button>
+          <div className="col-8 reg_center tooth" style={{ background: "#8BB6B3" }}>
             <img src="tooth.png" alt="" width={600} height={400} />
             {inpt.map((el, idx) => (
               <div className={`input b${idx + 1} d-grid`} key={idx}>
@@ -178,7 +205,10 @@ const Register = () => {
                 <div className="check__group py-2 ms-2 d-grid">
                   {
                     details.map((item, i) => (
-                      <FormControlLabel key={i} control={<Checkbox />} label={item} />
+                      <div className="list__item">
+                        <input type="checkbox" className="details_inp" value={item.id}/>
+                        <label>{item.name}</label>
+                      </div>
                     ))
                   }
                 </div>
@@ -211,19 +241,19 @@ const Register = () => {
               <form action="" className="w-100 py-4 px-3">
                 <div className="info_item">
                   <label htmlFor="name1">First name</label>
-                  <input type="text" id="name1" />
+                  <input type="text" id="first_name" />
                 </div>
                 <div className="info_item">
-                  <label htmlFor="name2">Last name</label>
-                  <input type="text" id="name2" />
+                  <label htmlFor="last_name">Last name</label>
+                  <input type="text" id="last_name" />
                 </div>
                 <div className="info_item">
-                  <label htmlFor="name3">Phone</label>
-                  <input type="text" id="name3" />
+                  <label htmlFor="phone">Phone</label>
+                  <input type="text" id="phone" />
                 </div>
                 <div className="info_item">
-                  <label htmlFor="name4">Address</label>
-                  <input type="text" id="name4" />
+                  <label htmlFor="address">Address</label>
+                  <input type="text" id="address" />
                 </div>
               </form>
             </div>
@@ -240,63 +270,6 @@ const Register = () => {
                 type="button"
                 className="btn btn-primary"
                 onClick={handleSave}
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-      <div
-        className="modal fade"
-        id="exampleModal2"
-        tabIndex="-1"
-        aria-labelledby="exampleModal2Label"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModal2Label">
-                Create Cart
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body p-0">
-              <form action="" className="d-grid p-4">
-                <TextField select label="Select Patient" className="mb-3"
-                  value={patient2} onChange={(e)=>setPatient2(e.target.value)}>
-                  { patient.map((el, id) => (
-                      <MenuItem key={id} value={el.id}>{el.first_name +"  "+el.last_name}</MenuItem>
-                  ))}
-                </TextField>
-                <TextField select label="Select Doctor"
-                    value={doc2} onChange={(e)=>setDoc2(e.target.value)}>
-                    { doctors.map((el, id) => (
-                        <MenuItem key={id} value={el.id}>{el.first_name +"  "+el.last_name}</MenuItem>
-                    ))}
-                </TextField>
-              </form>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                ref={closeRef2}
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button
-                className="btn btn-primary"
-                onClick={handleSaveCart}
               >
                 Save
               </button>
