895415103e13dfbe02ff01001ecd95f80393cfcc
Bobur96 <boburusmonxojayev@gmail.com>
2023-05-07 23:39:59 +0500

    doc get complete and header change complete

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -1,15 +1,12 @@
-import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import axios from "axios";
 
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import IconButton from "@mui/material/IconButton";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
-import Checkbox from "@mui/material/Checkbox";
-import TextField from "@mui/material/TextField";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -24,16 +21,18 @@ const Doctor = () => {
 
   const closeRef = useRef();
   const closeRef2 = useRef();
-  const [doc, setDoc] = useState("");
 
+  const [edit, setEdit] = useState(false);
   const [patient, setPatient] = useState([]);
-  const [details, setDetails] = useState([]);
   const [cleaning, setCleaning] = useState([]);
   const [fillings, setFillings] = useState([]);
-  const [extractions, setExtractions] = useState([]);
+  const [treatment, setTreatment] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(false);
   const [dComplains, setDComplains] = useState([]);
-  const [treatments, setTreatments] = useState([]);
-  const [historyDay, setHistoryDay] = useState([]);
+  const [historyId, setHistoryId] = useState(null);
+  const [extractions, setExtractions] = useState([]);
+  const [treatmentAns, setTreatmentAns] = useState([]);
+  const [treatmentteeth, setTreatmentteeth] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -44,59 +43,134 @@ const Doctor = () => {
       await getDentalComplaints();
       await getExtractions();
       await getFillings();
+      await getTreatments();
       setLoading(false);
     };
     response();
   }, []);
 
   const handleSubmit = async () => {
-    let patientAnswer = "";
-    let tablee = "";
+    console.log("submit");
     const toothAnswer = [];
     const deteilAnswer = [];
-    queryAll(".patient input").forEach((el) => {
-      if (el.checked) patientAnswer = el.value;
-    });
+    const fillingAnswer = [];
+    const cleaningAnswer = [];
+    const extractionAnswer = [];
+
     queryAll(".tooth input").forEach((el) => {
       if (el.checked) toothAnswer.push(el.value);
     });
-    queryAll(".details_inp").forEach((el) => {
+    queryAll(".complain").forEach((el) => {
       if (el.checked) deteilAnswer.push(el.value);
     });
+    queryAll(".filling").forEach((el) => {
+      if (el.checked) fillingAnswer.push(el.value);
+    });
+    queryAll(".cleaning").forEach((el) => {
+      if (el.checked) cleaningAnswer.push(el.value);
+    });
+    queryAll(".extraction").forEach((el) => {
+      if (el.checked) extractionAnswer.push(el.value);
+    });
+
+    const body = {
+      tooth_id: toothAnswer,
+      complaint_id: deteilAnswer,
+      treatment_id: treatment,
+      filling_id: fillingAnswer,
+      cleaning_agent_id: cleaningAnswer,
+      extraction_id: extractionAnswer,
+    };
+
+    const create_body = {
+      treatmentteeth: treatmentteeth,
+      tooth_id: toothAnswer,
+      complaint_id: deteilAnswer,
+    };
+
+    if (historyId) {
+      await axios
+        .put(
+          `${process.env.REACT_APP_API_URL}/doctor/update_history?treatmentteeth_id=${treatmentteeth}`,
+          body,
+          {
+            headers: { token: sessionStorage.getItem("token") },
+          }
+        )
+        .then((res) => {
+          toast.success("Succesfully saved!");
+          queryAll("input").forEach((el) => (el.checked = false));
+        })
+        .catch((err) => toast.error("Ann error occured!"));
+    } else {
+      await axios
+        .post(
+          `${process.env.REACT_APP_API_URL}/doctor/create_history`,
+          create_body,
+          {
+            headers: { token: sessionStorage.getItem("token") },
+          }
+        )
+        .then((res) => {
+          toast.success("Succesfully saved!");
+          queryAll("input").forEach((el) => (el.checked = false));
+        })
+        .catch((err) => toast.error("Ann error occured!"));
+    }
   };
 
   const getPatiens = () => {
     axios(`${process.env.REACT_APP_API_URL}/doctor/get_patients`, {
       headers: { token: sessionStorage.getItem("token") },
-    }).then((res) => setPatient(res.data))
+    })
+      .then((res) => setPatient(res.data))
       .catch((err) => console.log(err));
   };
 
   const getCleaning = () => {
-    axios(`${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=CleaningAgents`, {
-      headers: { token: sessionStorage.getItem("token") },
-    }).then((res) => setCleaning(res.data))
+    axios(
+      `${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=CleaningAgents`,
+      {
+        headers: { token: sessionStorage.getItem("token") },
+      }
+    )
+      .then((res) => setCleaning(res.data))
       .catch((err) => console.log(err));
   };
 
   const getFillings = () => {
-    axios(`${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=Fillings`, {
-      headers: { token: sessionStorage.getItem("token") },
-    }).then((res) => setFillings(res.data))
+    axios(
+      `${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=Fillings`,
+      {
+        headers: { token: sessionStorage.getItem("token") },
+      }
+    )
+      .then((res) => setFillings(res.data))
       .catch((err) => console.log(err));
   };
 
   const getExtractions = () => {
-    axios(`${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=Extractions`, {
-      headers: { token: sessionStorage.getItem("token") },
-    }).then((res) => setExtractions(res.data))
+    axios(
+      `${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=Extractions`,
+      {
+        headers: { token: sessionStorage.getItem("token") },
+      }
+    )
+      .then((res) => setExtractions(res.data))
       .catch((err) => console.log(err));
   };
 
   const getDentalComplaints = () => {
-    axios(`${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=DentalComplaints`, {
-      headers: { token: sessionStorage.getItem("token") },
-    }).then((res) => setDComplains(res.data))
+    axios(`${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=DentalComplaints`,
+      { headers: { token: sessionStorage.getItem("token") } }
+    ).then((res) => setDComplains(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  const getTreatments = () => {
+    axios(`${process.env.REACT_APP_API_URL}/doctor/get_obj?table_name=Treatments`,
+      { headers: { token: sessionStorage.getItem("token") } }
+    ).then((res) => setTreatment(res.data))
       .catch((err) => console.log(err));
   };
 
@@ -108,13 +182,71 @@ const Doctor = () => {
   };
 
   const handleChange = (id) => {
-    axios(`${process.env.REACT_APP_API_URL}/doctor/get_treatment_day?patient_id=${id}`,
+    setEdit(false);
+    setHistoryId(null);
+    setErrorMsg(false);
+    setTreatmentAns(null);
+    setTreatmentteeth(null);
+    axios(
+      `${process.env.REACT_APP_API_URL}/doctor/get_treatment_day?patient_id=${id}`,
       { headers: { token: sessionStorage.getItem("token") } }
-    ).then(res => {
-      setHistoryDay(res.data[0])
-      console.log(res.data[0])
-    }).catch(err => setHistoryDay([]))
-  }
+    )
+      .then((res) => {
+        setEdit(true);
+        console.log(res.data[0][0])
+        setHistoryId(res.data[0][0].id);
+        setTreatmentAns(res.data[0][0].treatment_id);
+        setTreatmentteeth(res.data[0][0].treatmentteeth);
+        queryAll(".reg_center input").forEach((el) => {
+          el.disabled = false;
+          el.checked = false;
+        });
+        queryAll(".details_inp").forEach((el) => {
+          el.disabled = false;
+          el.checked = false;
+        });
+        res.data[0][0].tooth_id?.forEach((el) => {
+          query(`.inp${el}`).checked = true;
+        });
+        res.data[0][0].complaint_id?.forEach((el) => {
+          query(`.inp_c${el}`).checked = true;
+        });
+        res.data[0][0].extraction_id?.forEach((el) => {
+          query(`.inp_ex${el}`).checked = true;
+        });
+        res.data[0][0].filling_id?.forEach((el) => {
+          query(`.inp_f${el}`).checked = true;
+        });
+        res.data[0][0].cleaning_agent_id?.forEach((el) => {
+          query(`.inp_cl${el}`).checked = true;
+        });
+        queryAll("input").forEach((el) => {
+          el.disabled = true;
+        });
+        queryAll('input[type="radio"]').forEach((el) => {
+          el.disabled = false;
+        });
+      })
+      .catch((err) => {
+        setErrorMsg(true);
+        setTreatmentteeth(null);
+        queryAll(".reg_center input").forEach((el) => {
+          el.disabled = false;
+          el.checked = false;
+        });
+        queryAll(".details_inp").forEach((el) => {
+          el.disabled = false;
+          el.checked = false;
+        });
+      });
+  };
+
+  const handleEdit = () => {
+    setEdit(false);
+    queryAll("input").forEach((inp) => {
+      inp.disabled = false;
+    });
+  };
 
   return (
     <div className="register">
@@ -133,7 +265,7 @@ const Doctor = () => {
                     name="patient"
                     id={`${item.id}`}
                     value={item.id}
-                    onChange={(e)=> handleChange(item.id)}
+                    onChange={(e) => handleChange(item.id)}
                   />
                   <label htmlFor={`${item.id}`}>
                     {item.first_name + "  " + item.last_name}
@@ -152,47 +284,69 @@ const Doctor = () => {
             </>
           )}
         </div>
-        <div className="col-9 row reg_content">
-          <div
-            className="col-8 reg_center tooth"
-            style={{ background: "#8BB6B3" }}
-          >
-            {
-              historyDay.length !== 0 &&
-              <>
-                <img src="tooth.png" alt="" width={600} height={400} />
-                {inpt.map((el, idx) => (
-                  historyDay?.tooth_id.map(id => (
-                    <div className={`input b${idx + 1} d-grid`} key={el}>
-                      <label htmlFor={`chkbox${el}`}>{el}</label>
-                      <input type="checkbox" id={`chkbox${el}`} defaultChecked={id === el} value={el} />
-                    </div>
-                  ))
-                ))}
-                {inpt2.map((el, idx) => (
-                  historyDay?.tooth_id.map(id => (
-                    <div className={`input bc c${idx + 1} d-grid`} key={el}>
-                      <input type="checkbox" id={`chkbox2${el}`} defaultChecked={id === el} value={el} />
-                      <label htmlFor={`chkbox2${el}`}>{el}</label>
-                    </div>
-                  ))
-                ))}
-              </>
-            }
 
-            <button
-              className="btn btn-primary position-absolute"
-              style={{ top: "490px", right: "265px", width: "145px" }}
-              onClick={handleSubmit}
+        <div className="col-9 row reg_content">
+          
+            <div
+              className="col-8 reg_center tooth"
+              style={{ background: "#8BB6B3" }}
             >
-              SUBMIT
-            </button>
-          </div>
+              {
+                errorMsg ? 
+                <div className="error">An error occurred</div> :
+                <>
+                  <img src="tooth.png" alt="" width={600} height={400} />
+                    {inpt.map((el, idx) => (
+                      <div className={`input b${idx + 1} d-grid`} key={el}>
+                        <label htmlFor={el}>{el}</label>
+                        <input
+                          type="checkbox"
+                          className={`inp${el}`}
+                          id={el}
+                          value={el}
+                        />
+                      </div>
+                    ))}
+                    {inpt2.map((el, idx) => (
+                      <div className={`input bc c${idx + 1} d-grid`} key={el}>
+                        <input
+                          type="checkbox"
+                          className={`inp${el}`}
+                          id={el}
+                          value={el}
+                        />
+                        <label htmlFor={el}>{el}</label>
+                      </div>
+                    ))}
+                    {edit && (
+                      <button
+                        className="btn btn-secondary position-absolute"
+                        style={{ top: "490px", right: "26px", width: "65px" }}
+                        onClick={() => handleEdit()}
+                      >
+                        edit
+                      </button>
+                    )}
+                    <button
+                      className="btn btn-primary position-absolute"
+                      style={{ top: "490px", right: "275px", width: "125px" }}
+                      onClick={handleSubmit}
+                    >
+                      SUBMIT
+                    </button> 
+                </>
+              }
+            </div>
+
           <div className="col-4 reg_info">
             <FormGroup>
               <h4 className="text-center my-4">Informatsiya</h4>
+
               <div className="check__group py-2 ms-2 d-grid">
-                <div className="accordion accordion-flush" id="accordionFlushExample">
+                <div
+                  className="accordion accordion-flush"
+                  id="accordionFlushExample"
+                >
                   <div className="accordion-item">
                     <h2 className="accordion-header" id="flush-headingOne">
                       <button
@@ -203,7 +357,7 @@ const Doctor = () => {
                         aria-expanded="false"
                         aria-controls="flush-collapseOne"
                       >
-                        DentalComplaints
+                        Dental Complaints
                       </button>
                     </h2>
                     <div
@@ -213,14 +367,14 @@ const Doctor = () => {
                       data-bs-parent="#accordionFlushExample"
                     >
                       <div className="accordion-body">
-                        {dComplains.map((details) => (
-                          <div className="list__item">
+                        {dComplains.map((itm) => (
+                          <div className="list__item" key={itm.id}>
                             <input
                               type="checkbox"
-                              className="details_inp"
-                              value={details.id}
+                              className={`details_inp complain inp_c${itm.id}`}
+                              value={itm.id}
                             />
-                            <label>{details.name}</label>
+                            <label>{itm.name}</label>
                           </div>
                         ))}
                       </div>
@@ -246,14 +400,14 @@ const Doctor = () => {
                       data-bs-parent="#accordionFlushExample"
                     >
                       <div className="accordion-body">
-                        {cleaning.map((details) => (
-                          <div className="list__item">
+                        {cleaning.map((itm) => (
+                          <div className="list__item" key={itm.id}>
                             <input
                               type="checkbox"
-                              className="details_inp"
-                              value={details.id}
+                              className={`details_inp cleaning inp_cl${itm.id}`}
+                              value={itm.id}
                             />
-                            <label>{details.name}</label>
+                            <label>{itm.name}</label>
                           </div>
                         ))}
                       </div>
@@ -279,14 +433,14 @@ const Doctor = () => {
                       data-bs-parent="#accordionFlushExample"
                     >
                       <div className="accordion-body">
-                        {fillings.map((details) => (
-                          <div className="list__item">
+                        {fillings.map((itm) => (
+                          <div className="list__item" key={itm.id}>
                             <input
                               type="checkbox"
-                              className="details_inp"
-                              value={details.id}
+                              className={`details_inp filling inp_f${itm.id}`}
+                              value={itm.id}
                             />
-                            <label>{details.name}</label>
+                            <label>{itm.name}</label>
                           </div>
                         ))}
                       </div>
@@ -312,26 +466,59 @@ const Doctor = () => {
                       data-bs-parent="#accordionFlushExample"
                     >
                       <div className="accordion-body">
-                        {extractions.map((details) => (
-                          <div className="list__item">
+                        {extractions.map((itm) => (
+                          <div className="list__item" key={itm.id}>
                             <input
                               type="checkbox"
-                              className="details_inp"
-                              value={details.id}
+                              className={`details_inp extraction inp_ex${itm.id}`}
+                              value={itm.id}
                             />
-                            <label>{details.name}</label>
+                            <label>{itm.name}</label>
                           </div>
                         ))}
                       </div>
                     </div>
                   </div>
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="flush-heading5">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapse5"
+                        aria-expanded="false"
+                        aria-controls="flush-collapse5"
+                      >
+                        Treatments
+                      </button>
+                    </h2>
+                    <div
+                      id="flush-collapse5"
+                      className="accordion-collapse collapse"
+                      aria-labelledby="flush-heading5"
+                      data-bs-parent="#accordionFlushExample"
+                    >
+                      <div className="accordion-body">
+                        {treatment?.map((itm) => (
+                          <div className="list__item" key={itm.id}>
+                            <input
+                              type="checkbox"
+                              className={`details_inp tratment inp_ex${itm.id}`}
+                              value={itm.id}
+                            />
+                            <label>{itm.name}</label>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+
                 </div>
               </div>
             </FormGroup>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,52 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
-import AdbIcon from '@mui/icons-material/Adb';
-import { NavLink } from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import AdbIcon from "@mui/icons-material/Adb";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Header() {
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            <NavLink to="/register">Register</NavLink>
-            <NavLink to="/doctor">Doctor</NavLink>
-            <NavLink to="/navbat">Navbat</NavLink>
-          </Box>
+  const navigate = useNavigate()
+  let token = sessionStorage.getItem("token")
+  let role = sessionStorage.getItem("role")
 
-          <Box sx={{ flexGrow: 0 }}>
-            <NavLink to="/login">Logout</NavLink>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("role");
+    token = ''; role = '';
+    navigate('/')
+  };
+
+  return (
+    <>
+      {token ? (
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {role == "register" && <NavLink to="/register">Register</NavLink>}
+                {role == "doctor" && <NavLink to="/doctor">Doctor</NavLink>}
+                <NavLink to="/navbat">Navbat</NavLink>
+              </Box>
+
+              <Box sx={{ flexGrow: 0 }}>
+                <button
+                  onClick={() => handleLogout()}
+                  className="btn btn-secondary"
+                >
+                  Logout
+                </button>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 export default Header;
